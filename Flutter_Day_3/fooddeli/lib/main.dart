a14fabad1dd4e9ee66318fd684368b52e84fc7c1
Trinity <56246225+Trinity6264@users.noreply.github.com>
2021-11-08 15:20:42 +0100

    preacaching image s from asset
    first we will put all the assets images in a list and loop through then put each individual in the precache images

--- Flutter_Day_3/fooddeli/lib/main.dart
+++ Flutter_Day_3/fooddeli/lib/main.dart
@@ -3,7 +3,28 @@
 import 'package:flutter/material.dart';
 import 'package:fooddeli/pages/start.dart';
 
+const List _allAsset = [
+  ///tabbar
+  'assets/images/Briyani.jpg',
+  'assets/images/Chapathi.jpg',
+  'assets/images/ChickenRice.jpg',
+  'assets/images/EggOmlete.jpg',
+  'assets/images/Noodles.jpg',
+  'assets/images/starter.jpg'
+];
+
+
 void main() {
+  final binding = WidgetsFlutterBinding.ensureInitialized();
+
+  binding.addPostFrameCallback((_) async {
+    BuildContext context = binding.renderViewElement;
+    if (context != null) {
+      for (var asset in _allAsset) {
+        precacheImage(AssetImage(asset), context);
+      }
+    }
+  });
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
